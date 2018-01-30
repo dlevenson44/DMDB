aaddbfe12368c5e81be0bdf68aafcb1cf2ea4763
Daniel Levenson <daniellevenson4@gmail.com>
2018-01-30 15:27:04 -0500

    set up header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+	return (
+		<div className="nav-container">
+			<nav>
+				<ul className="ul-container">
+					<Link to ='/'>
+					<button className="nav-item">Home</button>
+					</Link>
+
+					<Link to ='/login'>
+					<button className="nav-item">Login</button>
+					</Link>
+
+					<Link to ='/register'>
+					<button className="nav-item">Register</button>
+					</Link>
+
+					<Link to = '/dashboard'>
+					<button className="nav-item">Dashboard</button>
+					</Link>
+
+					<Link to = '/movies'>
+					<button className="nav-item">Movies</button>
+					</Link>
+				</ul>
+			</nav>
+		</div>
+		)
+}
