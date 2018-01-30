9a5b39509bbaa9c12c6739de552a5e6ae96d671b
Daniel Levenson <daniellevenson4@gmail.com>
2018-01-30 15:39:27 -0500

    set up app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,40 @@
+// import react and styling
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import react router
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+
+// import components
+import Header from './components/Header'
+import Home from './components/Home'
+import Footer from './components/Footer'
 
 class App extends Component {
+  constructor() {
+    super()
+    // set initial state
+    this.state = {
+      auth: false,
+      user: null,
+    }
+  }
+
+        // Below are routes that will be used when components are created
+          //   <Route path='/login' component={Login} />
+          // <Route path='/register' component={Register} />
+          // <Route path='/dashboard' component={Dashboard} />
+          // <Route path='/movies' component={Movies} />
+
+  // render data
   render() {
     return (
-
+      <Router>
+        <div className="App">
+          <Route exact path='/' component={Home} />
+          <Header />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,3 +30,5 @@ const Header = () => {
 		</div>
 		)
 }
+
+export default Header;
