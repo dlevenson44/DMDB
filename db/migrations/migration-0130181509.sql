582b5ae6980e37e4664bfce0252ac4ded15d2d9b
Daniel Levenson <daniellevenson4@gmail.com>
2018-01-30 15:14:50 -0500

    set up migration file

---
+++ db/migrations/migration-0130181509.sql
@@ -0,0 +1,16 @@
+-- users table schema
+CREATE TABLE IF NOT EXISTS users (
+	id SERIAL PRIMARY KEY,
+	username VARCHAR(225) UNIQUE NOT NULL,
+	password_digetst TEXT NOT NULL,
+	email VARCHAR(225) UNIQUE NOT NULL
+);
+
+-- movies table schema
+CREATE TABLE IF NOT EXISTS movies (
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(225),
+	description TEXT,
+	genre VARCHAR(225),
+	user_id INT REFERENCES users(id)
+);
