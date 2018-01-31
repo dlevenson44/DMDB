ad55f1d164619025bca95cd0d7180e0f8fe9d01c
Daniel Levenson <daniellevenson4@gmail.com>
2018-01-31 13:52:47 -0500

    updated movielist component

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Movie from './Movie'
+import MovieForm from './MovieForm'
+
 
 class MovieList extends Component {
 	constructor(props) {
@@ -6,11 +9,20 @@ class MovieList extends Component {
 		this.state = {
 			movies: null,
 			dataLoaded: false,
+			auth: props.auth,
+			currentlyEditing: null,
 		}
 		this.renderMovieList = this.renderMovieList.bind(this)
+		this.handleFormSubmit = this.handleFormSubmit.bind(this)
+		this.setEditing = this.setEditing.bind(this)
+		this.getAllMovies = this.getAllMovies.bind(this)
 	}
 
 	componentDidMount() {
+		this.getAllMovies()
+	}
+
+	getAllMovies() {
 		fetch('/api/movies', { credentials: 'include' })
 		.then(res => res.json())
 		.then(res => {
@@ -21,10 +33,34 @@ class MovieList extends Component {
 		}).catch(err => console.log(err))
 	}
 
+	setEditing(id) {
+		this.setStatE({
+			currentlyEditing: id,
+		})
+	}
+
+	handleFormSubmit(method, e, data, id) {
+		e.preventDefualt()
+		fetch(`/api/movies/${id || ''}`, {
+			method: method,
+			credentials: 'include',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringifiy(data),
+		)}.then(res => res.json())
+		.then(res => {
+			console.log(res)
+			this.getAllMovies()
+		}).catch(err => console.log(err))
+	}
+
 	renderMovieList() {
 		if (this.state.dataLoaded) {
 			return this.state.movies.map(movie => {
-				return <Movie key={movie.id} movie={movie} />
+				if (movie.id === this.state.currentlyEditing)
+					return <MovieForm movie={movie} handleFormSubmit={this.handleFormSubmit} isAdd={false} key={movie.id} />
+				else return <Movie key={movie.id} movie={movie} auth={this.state.auth} setEditing={this.setEditing} />
 			})
 		} else return <p> Loading!</p>
 	}
@@ -32,6 +68,9 @@ class MovieList extends Component {
 	render() {
 		return(
 			<div className="movielist">
+				{this.state.auth
+					? <MovieForm isAdd={true} handleFormSubmit={this.handleFormSubmit} />
+					: ''}
 				{this.renderMovieList()}
 			</div>
 			)
