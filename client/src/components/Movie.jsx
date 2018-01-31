454fb6366231b2415a5a5f8b2360ad3cd58eb8a7
Daniel Levenson <daniellevenson4@gmail.com>
2018-01-31 18:01:08 -0500

    redid movie component

--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 const Movie = (props) => {
 	return(
 		<div className="movie">
-			<p>{props.movie.title}</p>
-			<p>{props.movie.genre}</p>
-			<p>{props.movie.description}</p>
-			{props.auth
-				? <span className="edit" onClick={() => props.setEditing(props.movie.id)}>Edit</span>
-				: ''}
+			<h1 className="movie-header">{props.currentMovie.title}</h1>
+			<h3 className="movie-subhead">{props.currentMovie.genre}</h1>
+			<Link to={`/movies/${props.currentMovie.id}`}>See More</Link>
 		</div>
 	)
 }
