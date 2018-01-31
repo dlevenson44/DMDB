35d36fad42b1422031729e9dd0636f957b171ef7
Daniel Levenson <daniellevenson4@gmail.com>
2018-01-31 13:06:17 -0500

    set up movie controller

---
+++ controllers/movies-controller.js
@@ -0,0 +1,67 @@
+//bring in movie model
+const Movie = require('../models/Movie')
+
+// initiate movieController object
+const movieController = {};
+
+// movie index function, displays all movies
+movieController.index = (req, res, next) => {
+	Movie.findAl()
+	.then(movies => {
+		res.json({
+			message: 'ok',
+			data: { movies },
+		})
+	}).catch(next)
+}
+
+// movie show function, displays specific movie
+movieController.show = (req, res, next) => {
+	Movie.findById(req.params.id)
+	.then(movie => {
+		res.json({
+			message: 'ok',
+			data: { movie },
+		})
+	}).catch(next)
+}
+
+// create movie function to add new entries
+movieController.create = (req, res, next) => {
+	Movie.create({
+		title: req.body.title,
+		description: req.body.description,
+		genre: req.body.genre,
+	}, req.user.id).then(movie => {
+		res.json({
+			message: 'Successfully added movie',
+			data: { movie },
+		})
+	}).catch(next)
+}
+
+// update existing movie entry
+movieController.update = (req, res, next) => {
+	Movie.update({
+		title: req.body.title,
+		description: req.body.description,
+		genre: req.body.genre,
+	}, req.params.id).then(movie => {
+		res.json({
+			message: 'Successfully updated movie',
+			data: { movie },
+		})
+	}).catch(next)
+}
+
+// delete movie entry
+movieController.delete = (req, res, next) => {
+	Movie.destroy(req.params.id)
+	.then(() => {
+		res.json({
+			message: 'Successfully deleted movie',
+		})
+	}).catch(next)
+}
+
+module.exports = movieController;
