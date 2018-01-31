18eb349bf7d902a7a048752b412c0173e4431b58
Daniel Levenson <daniellevenson4@gmail.com>
2018-01-31 12:57:26 -0500

    set up movie model

---
+++ models/Movie.js
@@ -0,0 +1,50 @@
+// require config file
+const db = require('../db/config')
+
+// initiate movie object
+const Movie = {};
+
+// find all movies query
+Movie.findAll = () => {
+	return db.query('SELECT * FROM moves');
+}
+
+// find one movie
+Movie.findById = (id) => {
+	return db.oneOrNone(`
+		SELECT * FROM movies
+		WHERE id = $1
+		`, [id]);
+}
+
+// create new movie
+Movie.create = (movie, userId) => {
+	return db.one(`
+		INSERT INTO movies
+		(title, description, genre, user_id)
+		VALUES ($1, $2, $3, $4)
+		RETURNING *
+		`, [movie.title, movie.description, movie.genre, userId]);
+}
+
+// update existing movie
+Movie.update = (movie, id) => {
+	return db.one(`
+		UPDATE movies SET
+		title = $1, 
+		description = $2, 
+		genre = $3,
+		WHERE id = $4
+		RETURNING *
+		`, [movie.title, movie.description, movie.genre, id]);
+}
+
+// delete movie
+Movie.destroy = (id) => {
+	return db.none(`
+		DELETE FROM movies
+		WHERE ID = $1
+		`, [id]);
+}
+
+module.exports = Movie;
