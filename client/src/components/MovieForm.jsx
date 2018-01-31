dcd025e8fa1520b19969845b2fcd06c8953d7ee3
Daniel Levenson <daniellevenson4@gmail.com>
2018-01-31 17:52:31 -0500

    minor changes to movieform

--- client/src/components/MovieForm.jsx
+++ client/src/components/MovieForm.jsx
@@ -8,11 +8,9 @@ class MovieForm extends Component {
 			title: props.movie ? props.movie.title : '',
 			genre: props.movie ? props.movie.genre : '',
 			description: props.movie ? props.movie.description : '',
-			fireRedirect: false,
-			redirectPath: null,
 		}
 		this.handleChange = this.handleChange.bind(this)
-		this.handleFormSubmit = this.handleFormSubmit.bind(this)
+		// this.handleFormSubmit = this.handleFormSubmit.bind(this)
 	}
 
 	handleChange(e) {
@@ -50,15 +48,17 @@ class MovieForm extends Component {
 
 	render() {
 		return(
-			<form onSubmit={(this.props.isAdd
-				? (e) => this.handleFormSubmit('POST', e, this.state)
-				: (e) => this.handleFormSubmit('PUT', e, this.state, this.props.movie.id)
-				)}>
-				<input type="text" name="title" placeholder="Title" value={this.state.title} onChange={this.handleChange} />
-				<input type="text" name="description" placeholder="Description" value={this.state.description} onChange={this.handleChange} />
-				<input type="text" name="genre" placeholder="Genre" value={this.state.genre} onChange={this.handleChange} />
-				<input type="submit" value={this.props.isAdd ? 'Add it!' : 'Edit it!'} />
-			</form>
+			<div className="form">
+				<form onSubmit={(this.props.isAdd
+					? (e) => this.handleFormSubmit('POST', e, this.state)
+					: (e) => this.handleFormSubmit('PUT', e, this.state, this.props.movie.id)
+					)}>
+					<input type="text" name="title" placeholder="Title" value={this.state.title} onChange={this.handleChange} />
+					<input type="text" name="description" placeholder="Description" value={this.state.description} onChange={this.handleChange} />
+					<input type="text" name="genre" placeholder="Genre" value={this.state.genre} onChange={this.handleChange} />
+					<input type="submit" value={this.props.isAdd ? 'Add it!' : 'Edit it!'} />
+				</form>
+			</div>
 			)
 	}
 }
