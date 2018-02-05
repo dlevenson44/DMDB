2e8e16e10c6f9b1a43e2dfc6fed39ce1c7401dfa
Daniel Levenson <daniellevenson4@gmail.com>
2018-02-05 17:46:41 -0500

    reverted db change

--- db/config.js
+++ db/config.js
@@ -9,7 +9,7 @@ const pgp = require('pg-promise')(options);
 function setDatabase() {
   if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
     return pgp({
-      database: 'dmdb',
+      database: 'movies_app',
       port: 5432,
       host: 'localhost',
     })
