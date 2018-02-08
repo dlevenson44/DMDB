f525b6b9f22a8bfd8da6033f1b0c0d897cd224ed
Daniel Levenson <daniellevenson4@gmail.com>
2018-02-08 16:28:44 -0500

    finished styling for desktop

--- client/src/components/MovieSingle.jsx
+++ client/src/components/MovieSingle.jsx
@@ -5,17 +5,21 @@ import { Link } from 'react-router-dom'
 const MovieSingle = (props) => {
 	console.log(props, 'from movie single')
 	return(
-		<div className="single-container">
-			<div className="movie-single">
-				<img src={props.currentMovie.image} />
-				<h1 className="movie-header">{props.currentMovie.title}</h1>
-				<h3 className="movie-subhead">{props.currentMovie.genre}</h3>
-				<p className="movie-desc">{props.currentMovie.description}</p>
+		<div>
+			<div className="single-container">			
+				<div>
+					<img src={props.currentMovie.image} className="movie-img" />
+				</div>
+				<div className="movie-single">				
+					<h1 className="text-lg-center" id="single-header">{props.currentMovie.title}</h1>
+					<h3 className="text-md-center" id="single-genre">{props.currentMovie.genre}</h3>
+					<p className="movie-desc">{props.currentMovie.description}</p>
+				</div>
 			</div>
 			{props.auth
 				? (<div className="movie-links">
-					<Link to={`/movies/edit/${props.currentMovie.id}`}>Edit</Link>
-					<button className="delete" onClick={() => props.movieDelete(props.currentMovie.id)}>Delete</button>
+					<Link to={`/movies/edit/${props.currentMovie.id}`} className="single-link"><button id="nav">Edit Post</button></Link>
+					<button id="nav" onClick={() => props.movieDelete(props.currentMovie.id)}>Delete Post</button>
 				</div>) : (
 				<div>
 					<p hidden>Unauthorized</p>
